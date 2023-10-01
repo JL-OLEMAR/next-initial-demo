1eedad13571c25f1ce2107cd8a9e5fe82b091987
JL-OLEMAR <joseluis19963@gmail.com>
2023-10-01 03:15:00 -0500

    feat: Set the theme using cookies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,42 @@
-import '@/styles/globals.css'
+import { useEffect, useState } from 'react'
 import type { AppProps } from 'next/app'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import { darkTheme } from '@/themes'
+import { CssBaseline, Theme, ThemeProvider } from '@mui/material'
+import Cookies from 'js-cookie'
 
-export default function App({ Component, pageProps }: AppProps) {
+import { customTheme, darkTheme, lightTheme } from '@/themes'
+import '@/styles/globals.css'
+
+interface Props extends AppProps {
+  theme: string
+}
+export default function App({ Component, pageProps, theme = 'dark' }: Props) {
+  const [currentTheme, setCurrentTheme] = useState<Theme>(lightTheme)
+
+  useEffect(() => {
+    const cookieTheme = Cookies.get('theme') ?? 'light'
+    const selectedTheme = cookieTheme === 'light'
+      ? lightTheme
+      : (cookieTheme === 'dark') ? darkTheme : customTheme
+
+    setCurrentTheme(selectedTheme)
+  }, [])
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={currentTheme}>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
   )
 }
+
+// App.getInitialProps = async (appContext: AppContext) => {
+//   const { theme } = appContext.ctx.req
+//     ? (appContext.ctx.req as any).cookies
+//     : { theme: 'light' }
+
+//   const validThemes = ['light', 'dark', 'custom']
+
+//   return {
+//     theme: validThemes.includes(theme) ? theme : 'light'
+//   }
+// }
--- pages/theme-changer.tsx
+++ pages/theme-changer.tsx
@@ -1,5 +1,9 @@
-import { type ChangeEvent, useState } from 'react'
+import { type ChangeEvent, useState, useEffect } from 'react'
+import { type GetServerSideProps } from 'next'
+import axios from 'axios'
+import Cookies from 'js-cookie'
 import {
+  Button,
   Card,
   CardContent,
   FormControl,
@@ -10,11 +14,31 @@ import {
 } from '@mui/material'
 import { Layout } from '@/components/layouts'
 
-export default function ThemeChanger() {
-  const [currentTheme, setCurrentTheme] = useState('light')
+interface ThemeChangerProps {
+  theme: string
+}
+
+export default function ThemeChanger({ theme }: ThemeChangerProps) {
+  const [currentTheme, setCurrentTheme] = useState(theme)
+  console.log({ theme })
+
+  useEffect(() => {
+    console.log('LocalStorage---> ', localStorage.getItem('theme'))
+    console.log('Cookies---> ', Cookies.get('theme'))
+  }, [])
 
   const onThemeChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setCurrentTheme(event.target.value)
+    const selectedTheme = event.target.value
+    console.log({ selectedTheme })
+    setCurrentTheme(selectedTheme)
+
+    localStorage.setItem('theme', selectedTheme)
+    Cookies.set('theme', selectedTheme)
+  }
+
+  const handleClick = async () => {
+    const { data } = await axios.get('/api/hello')
+    console.log({ data })
   }
 
   return (
@@ -44,8 +68,29 @@ export default function ThemeChanger() {
               />
             </RadioGroup>
           </FormControl>
+
+          <Button
+            onClick={() => { void handleClick() }}
+            type='button'
+          >
+            Request
+          </Button>
         </CardContent>
       </Card>
     </Layout>
   )
+}
+
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const { theme = 'light', name = 'No name' } = req.cookies
+  const validThemes = ['light', 'dark', 'custom']
+
+  return {
+    props: {
+      theme: validThemes.includes(theme) ? theme : 'dark',
+      name
+    }
+  }
 }
